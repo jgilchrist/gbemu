6a1a3c40b6b7917172f79b2edacee28eb750efd4
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-12-14 23:30:07 +0000

    Make the MMU use the new Address type

--- src/MMU.h
+++ src/MMU.h
@@ -3,16 +3,17 @@
 
 #include <cstdint>
 #include "Memory.h"
+#include "Address.h"
 
 class MMU {
 public:
     MMU();
 
-    uint8_t read_byte(const uint16_t address) const;
-    uint16_t read_word(const uint16_t address) const;
+    uint8_t read_byte(const Address address) const;
+    uint16_t read_word(const Address address) const;
 
-    void write_byte(const uint16_t address, const uint8_t byte) const;
-    void write_word(const uint16_t address, const uint16_t word) const;
+    void write_byte(const Address address, const uint8_t byte) const;
+    void write_word(const Address address, const uint16_t word) const;
 
 private:
     Memory memory;
