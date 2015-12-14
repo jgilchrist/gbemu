f9d5f86036961bb7efc424f991b52a0ca8460eea
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-12-14 23:47:02 +0000

    Add stub implementations of some MMU functions

--- src/MMU.cc
+++ src/MMU.cc
@@ -1,1 +1,21 @@
 #include "MMU.h"
+
+MMU::MMU() {
+
+}
+
+uint8_t MMU::read_byte(const Address address) const {
+    return 0;
+}
+
+uint16_t MMU::read_word(const Address address) const {
+    return 0;
+}
+
+void MMU::write_byte(const Address address, const uint8_t byte) const {
+
+}
+
+void MMU::write_word(const Address address, const uint16_t word) const {
+
+}
