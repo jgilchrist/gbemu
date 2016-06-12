0ae863aa45b829a1030a0917e7e4f8dc8274aac6
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-06-12 18:29:12 +0100

    Remove extraneous ; on function definition

--- src/bitwise.h
+++ src/bitwise.h
@@ -4,11 +4,11 @@
 
 inline uint16_t compose_bytes(const uint8_t low, const uint8_t high) {
     return (high << 8) + low;
-};
+}
 
 inline bool check_bit(const uint8_t value, int bit) {
     return (value & (1 << bit)) != 0;
-};
+}
 
 inline uint8_t set_bit(const uint8_t value, int bit) {
     return static_cast<uint8_t>(value | (1 << bit));
