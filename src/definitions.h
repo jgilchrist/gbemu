09f52e6d6ca4025a846e665827371711d0af87f4
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-06-28 20:58:31 +0100

    Do not align = signs in consecutive statements

--- src/definitions.h
+++ src/definitions.h
@@ -4,9 +4,9 @@
 
 using uint = unsigned int;
 
-using u8  = uint8_t;
+using u8 = uint8_t;
 using u16 = uint16_t;
-using s8  = int8_t;
+using s8 = int8_t;
 
 const int GAMEBOY_WIDTH = 160;
 const int GAMEBOY_HEIGHT = 144;
