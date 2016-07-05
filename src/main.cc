9c2606f6481f8038c7de32fd019180184fe492ba
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-07-05 16:10:08 +0100

    Reorganise main.cc
    Remove unused imports, return error code when no argument provided,
    re-order statements.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,3 @@
-#include <cstdio>
-
 #include "gameboy.h"
 #include "cartridge.h"
 #include "video/screen.h"
@@ -9,19 +7,18 @@
 #include "video/screens/sfml_screen.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        log_error("Please provide a ROM file to run")
-        return 0;
-    }
-
     log_set_level(LogLevel::Trace);
 
+    if (argc != 2) {
+        log_error("Please provide a ROM file to run")
+        return 1;
+    }
+
     std::string rom_name = argv[1];
-    log_info("Loading rom from file: %s", rom_name.c_str());
-
     Cartridge cartridge(rom_name);
-    SFMLScreen screen;
-    Gameboy gameboy(screen, cartridge);
 
+    SFMLScreen screen;
+
+    Gameboy gameboy(screen, cartridge);
     gameboy.run();
 }
