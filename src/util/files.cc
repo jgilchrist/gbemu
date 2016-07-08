f5193eb70b76328c2394bc9796d8c5e4446b7ff1
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-07-08 15:48:33 +0100

    Provide a more descriptive error if the ROM file cannot be read

--- src/util/files.cc
+++ src/util/files.cc
@@ -1,5 +1,6 @@
 #include "files.h"
 
+#include "log.h"
 #include <fstream>
 
 std::vector<char> read_bytes(const std::string filename) {
@@ -9,6 +10,11 @@ std::vector<char> read_bytes(const std::string filename) {
     ifstream stream(filename.c_str(), ios::binary|ios::ate);
     ifstream::pos_type position = stream.tellg();
     size_t file_size = static_cast<size_t>(position);
+
+    if (file_size == -1) {
+        log_error("Cannot read from file: %s", filename.c_str());
+        exit(1);
+    }
 
     std::vector<char> result(file_size);
 
