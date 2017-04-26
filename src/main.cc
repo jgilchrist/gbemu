ff25143dbe1236490a54e3e91ad82f261a02157c
Jonny Gilchrist <jonnygilchrist@gmail.com>
2017-04-26 21:28:00 +0100

    Move the get_screen function and screen imports into its own header

--- src/main.cc
+++ src/main.cc
@@ -1,28 +1,18 @@
 #include "gameboy.h"
 #include "cartridge.h"
 #include "video/screen.h"
+#include "video/screens.h"
 #include "util/cli.h"
 #include "util/log.h"
 
-#include "video/screens/null_screen.h"
-#include "video/screens/sfml_screen.h"
-
 #include <memory>
-
-std::shared_ptr<Screen> get_screen(Options& options) {
-    if (options.headless) {
-        return std::make_shared<NullScreen>();
-    }
-
-    return std::make_shared<SFMLScreen>(options.show_full_framebuffer);
-}
 
 int main(int argc, char* argv[]) {
     Options options = get_options(argc, argv);
     log_set_level(get_log_level(options));
 
     Cartridge cartridge(options.filename);
-    auto screen = get_screen(options);
+    auto screen = get_screen(options.headless, options.show_full_framebuffer);
 
     Gameboy gameboy(screen, cartridge, options.debugger);
     gameboy.run();
---
+++ src/video/screens.h
@@ -0,0 +1,11 @@
+#include "screens/null_screen.h"
+#include "screens/sfml_screen.h"
+
+std::shared_ptr<Screen> get_screen(bool headless, bool show_full_framebuffer);
+std::shared_ptr<Screen> get_screen(bool headless, bool show_full_framebuffer) {
+    if (headless) {
+        return std::make_shared<NullScreen>();
+    }
+
+    return std::make_shared<SFMLScreen>(show_full_framebuffer);
+}
