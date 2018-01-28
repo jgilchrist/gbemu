55926d08df1639f521d9522a9359e50d56e23c06
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2018-01-28 17:35:58 +0000

    Turn add_warnings into a function
    This is preferable to having the file execute when imported

--- cmake/add_warnings.cmake
+++ cmake/add_warnings.cmake
@@ -1,33 +1,35 @@
-if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  set(extra_warn
-    -Weverything
-    -Wno-weak-vtables
-    -Wno-exit-time-destructors
-    -Wno-gnu-zero-variadic-macro-arguments
-    -Wno-global-constructors
-    -Wno-c++98-compat
-    -Wno-c++98-compat-pedantic)
-else()
-  set(extra_warn
-    -Wno-variadic-macros
-    -Wno-unknown-pragmas
-    -Wno-unused-variable)
-endif()
+function(add_warnings)
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(extra_warn
+        -Weverything
+        -Wno-weak-vtables
+        -Wno-exit-time-destructors
+        -Wno-gnu-zero-variadic-macro-arguments
+        -Wno-global-constructors
+        -Wno-c++98-compat
+        -Wno-c++98-compat-pedantic)
+    else()
+    set(extra_warn
+        -Wno-variadic-macros
+        -Wno-unknown-pragmas
+        -Wno-unused-variable)
+    endif()
 
-set(all_warn
-  ${extra_warn}
-  -Wall
-  -Wextra
-  -Wpedantic
-  -Wno-padded
-  -Wno-format-nonliteral
-  -Wno-unused-parameter)
+    set(all_warn
+    ${extra_warn}
+    -Wall
+    -Wextra
+    -Wpedantic
+    -Wno-padded
+    -Wno-format-nonliteral
+    -Wno-unused-parameter)
 
-# FIXME: Temporarily silence warnings during development
-if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  list(APPEND all_warn -Wno-missing-noreturn)
-else()
-  list(APPEND all_warn -Wno-return-type)
-endif()
+    # FIXME: Temporarily silence warnings during development
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    list(APPEND all_warn -Wno-missing-noreturn)
+    else()
+    list(APPEND all_warn -Wno-return-type)
+    endif()
 
-add_definitions(${all_warn})
+    add_definitions(${all_warn})
+endfunction()
