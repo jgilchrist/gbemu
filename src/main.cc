b168d722a9d003c849a0b831961db9b635a6417f
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2018-09-22 22:52:11 +0100

    Move screens.h into main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,20 @@
 #include "gameboy.h"
 #include "cartridge/cartridge.h"
 #include "video/screen.h"
-#include "video/screens.h"
+#include "video/screens/null_screen.h"
+#include "video/screens/sfml_screen.h"
 #include "util/cli.h"
 #include "util/log.h"
 
 #include <memory>
+
+std::shared_ptr<Screen> get_screen(std::shared_ptr<Input> input, Options& options) {
+    if (options.headless) {
+        return std::make_shared<NullScreen>();
+    }
+
+    return std::make_shared<SFMLScreen>(input, options.show_full_framebuffer);
+}
 
 int main(int argc, char* argv[]) {
     Options options = get_options(argc, argv);
