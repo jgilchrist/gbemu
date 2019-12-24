dce9901e0c73fa4c155f39bd0260b536c2322e2e
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2019-12-24 22:34:04 +0000

    Turn off -Wsign-conversion for development

--- cmake/add_warnings.cmake
+++ cmake/add_warnings.cmake
@@ -8,22 +8,28 @@ function(add_warnings)
         -Wno-gnu-zero-variadic-macro-arguments
         -Wno-global-constructors
         -Wno-c++98-compat
-        -Wno-c++98-compat-pedantic)
+        -Wno-c++98-compat-pedantic
+    )
     else()
     set(extra_warn
         -Wno-variadic-macros
         -Wno-unknown-pragmas
-        -Wno-unused-variable)
+        -Wno-unused-variable
+    )
     endif()
 
     set(all_warn
-    ${extra_warn}
-    -Wall
-    -Wextra
-    -Wpedantic
-    -Wno-padded
-    -Wno-format-nonliteral
-    -Wno-unused-parameter)
+        ${extra_warn}
+        -Wall
+        -Wextra
+        -Wpedantic
+        -Wno-padded
+        -Wno-format-nonliteral
+        -Wno-unused-parameter
+    )
+
+    # FIXME: Temporarily silenced during development
+    list(APPEND all_warn -Wno-sign-conversion)
 
     # FIXME: Temporarily silence warnings during development
     if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
